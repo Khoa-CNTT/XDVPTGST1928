06038723e3f9aae0845327b510e876791a188955
NVD-D4T <datnguyenvan.nvd@gmail.com>
2025-04-01 11:00:38 +0700

    test_inventory

--- Assets/Scripts/SaveScript.cs
+++ Assets/Scripts/SaveScript.cs
@@ -14,14 +14,25 @@ public class SaveScript : MonoBehaviour
     void Start()
     {
         weaponsPickedUp[0] = true;
+        weaponsPickedUp[1] = true;
+        weaponsPickedUp[2] = true;
+        weaponsPickedUp[3] = true;
+        weaponsPickedUp[4] = true;
+        weaponsPickedUp[5] = true;
         weaponsPickedUp[6] = true;
         weaponsPickedUp[7] = true;
+
 
         itemsPickedUp[0] = true;
         itemsPickedUp[1] = true;
         itemsPickedUp[2] = true;
         itemsPickedUp[3] = true;
-        itemsPickedUp[9] = true;
+        itemsPickedUp[4] = true;
+        itemsPickedUp[5] = true;
+        itemsPickedUp[6] = true;
+        itemsPickedUp[7] = true;
+
+        
     }
 
     // Update is called once per frame
