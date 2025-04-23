49dffb7cae2abced195994a3be4e6593eebc05e4
d4tnv2602 <datnguyenvan.nvd@gmail.com>
2025-04-23 17:16:15 +0700

    Animation_Zombie

--- Assets/Scripts/ZombieScript.cs
+++ Assets/Scripts/ZombieScript.cs
@@ -22,15 +22,22 @@ public class ZombieScript : MonoBehaviour
     public ZombieState chooseState;
     public float yAdiustment = 0.0f;
     private Animator anim;
-
+    private AnimatorStateInfo animInfo;
+    private NavMeshAgent agent;
     public bool randomState = false;
     public float randomTiming = 5f;
     private int newState = 0;
     private int currentState;
+    private GameObject[] targets;
+    private float[] walkSpeed = {0.15f, 1.0f, 0.75f};
+    private float distanceToTarget;
+    private int currentTarget = 0;
 
     void Start()
     {
         anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        targets = GameObject.FindGameObjectsWithTag("Target");
         anim.SetLayerWeight(((int)zombieStyle + 1), 1);
         if(zombieStyle == ZombieType.shuffle)
         {
@@ -42,11 +49,35 @@ public class ZombieScript : MonoBehaviour
         {
             InvokeRepeating("SetAnimState", randomTiming, randomTiming);
         }
+        agent.destination = targets[0].transform.position;
+        agent.speed = walkSpeed[(int)zombieStyle];
     }
 
     // Update is called once per frame
     void Update()
     {
+        distanceToTarget = Vector3.Distance(transform.position, targets[currentTarget].transform.position);
+        animInfo = anim.GetCurrentAnimatorStateInfo((int)zombieStyle);
+
+        if(animInfo.IsTag("motion"))
+        {
+            if(anim.IsInTransition((int)zombieStyle))
+            {
+                agent.isStopped = true;
+                
+            }
+        }
+        if(chooseState == ZombieState.Walking)
+        {
+            
+            if(distanceToTarget < 1.5f)
+            {
+                if(currentTarget < targets.Length - 1)
+                {
+                    currentTarget = Random.Range(0, targets.Length);
+                }
+            }
+        }
         
     }
 
@@ -61,14 +92,17 @@ public class ZombieScript : MonoBehaviour
         }
     }
     
-    public void WalkOff()
-    {
-        Debug.Log("WalkOff event triggered from animation!");
-    }
-
     public void WalkOn()
     {
-        Debug.Log("WalkOn event triggered from animation!");
+        agent.isStopped = false;
+        agent.destination = targets[currentTarget].transform.position;
     }
+    public void WalkOff()
+    {
+        agent.isStopped = true;
+    }
+
     
+    
+
 }
